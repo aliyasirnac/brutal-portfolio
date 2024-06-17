8ab6e70bd369bc0cd8026add4fb01fc39a4d5f5b
Ali Yasir Naç <76746351+aliyasirnac@users.noreply.github.com>
2024-06-17 20:08:01 +0300

    sanity added

--- astro.config.ts
+++ astro.config.ts
@@ -1,20 +1,20 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import UnoCSS from "unocss/astro";
+import sanity from "@sanity/astro";
+import react from "@astrojs/react";
 
+// https://astro.build/config
 export default defineConfig({
   // used to generate images
-  site:
-    process.env.VERCEL_ENV === "production"
-      ? "https://aliyasirnac.com/"
-      : process.env.VERCEL_URL
-        ? `https://${process.env.VERCEL_URL}/`
-        : "https://localhost:3000/",
+  site: process.env.VERCEL_ENV === "production" ? "https://aliyasirnac.com/" : process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}/` : "https://localhost:3000/",
   trailingSlash: "ignore",
-  integrations: [sitemap(), UnoCSS({ injectReset: true })],
+  integrations: [sitemap(), UnoCSS({
+    injectReset: true
+  }), sanity(), react()],
   vite: {
     optimizeDeps: {
-      exclude: ["@resvg/resvg-js"],
-    },
-  },
+      exclude: ["@resvg/resvg-js"]
+    }
+  }
 });
